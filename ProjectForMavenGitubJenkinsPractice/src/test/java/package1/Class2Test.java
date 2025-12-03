b62e5c8956892e752c73ec039756fbb1538aae63
Darshan <Darshan@LAPTOP-LP0NT2PF>
2025-12-03 17:31:46 +0530

    My first Commit

---
+++ ProjectForMavenGitubJenkinsPractice/src/test/java/package1/Class2Test.java
@@ -0,0 +1,15 @@
+package package1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Class2Test {
+	
+	@Test
+	public void class1() {
+		
+		Reporter.log("------------------Hi I'm Moulya H K--------------------------", true);
+		
+	}
+
+}
